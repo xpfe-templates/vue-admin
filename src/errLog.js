47a7c2e4be0a7b5f05df7eee8e6edd104ddb8c0c
xiaoping <edwardhjp@gmail.com>
2017-08-30 20:24:07 +0800

    Remv test code

--- src/errLog.js
+++ src/errLog.js
@@ -9,15 +9,6 @@
 import Vue from 'vue'
 import errLog from '@/store/errLog'
 
-Vue.config.errorHandler = (err, vm) => {
-  console.log(err, window.location.href)
-  errLog.pushLog({
-    err,
-    url: window.location.href,
-    vm
-  })
-}
-
 // 生产环境错误日志
 if (process.env.NODE_ENV === 'production') {
   Vue.config.errorHandler = (err, vm) => {
