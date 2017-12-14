ba33c077a2274f38a98211225168a045ab993717
xiaoping <edwardhjp@gmail.com>
2017-12-14 10:59:47 +0800

    update 更新error捕获

--- src/errLog.js
+++ src/errLog.js
@@ -2,14 +2,15 @@
  * @author xiaoping
  * @email edwardhjp@gmail.com
  * @create date 2017-08-24 10:54:35
- * @modify date 2017-08-24 10:54:35
+ * @modify date 2017-12-14 10:58:18
  * @desc [错误日志]
 */
 
 import Vue from 'vue'
+import { on } from 'xp-dom'
 import errLog from '@/store/errLog'
 
-// 生产环境错误日志
+// Vue错误日志
 if (process.env.NODE_ENV === 'production') {
   Vue.config.errorHandler = (err, vm) => {
     console.log(err, window.location.href)
@@ -20,3 +21,17 @@ if (process.env.NODE_ENV === 'production') {
     })
   }
 }
+
+// Script error
+on(window, 'error', (msg, url, line, col, error) => {
+  console.log('错误信息:', msg)
+  console.log('出错文件:', url)
+  console.log('出错行号:', line)
+  console.log('出错列号:', col)
+  console.log('错误详情:', error)
+})
+
+// Promise reject
+on(window, 'unhandledrejection', (e) => {
+  console.log('Promise not catch:', e.reason)
+})
