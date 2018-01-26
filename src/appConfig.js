8915509eb963b341da7fa3a07e67051842231f4b
xiaoping <edwardhjp@gmail.com>
2018-01-26 19:54:20 +0800

    update 更新appConfig

--- src/appConfig.js
+++ src/appConfig.js
@@ -8,31 +8,35 @@
 
 const env = process.env.NODE_ENV
 
-const gitDir = 'vue-admin' // 用户自动部署目录，必须和git目录或者jenkins的创建目录一致，影响history路由
+// 用户自动部署目录，必须和git目录或者jenkins的创建目录一致，影响路由
+const gitDir = 'vue-admin'
+let htmlTitle = 'Vue Admin - dev'
+
+// auth相关
 const clientId = '' // 应用名称
 const redirectUri = '' // auth登录跳转链接
 const authCodes = [2003] // 没有权限的错误码
+let authUrl = 'http://116.62.148.115:8082' // auth接口
 
-let htmlTitle = 'Vue Admin - dev'
-let baseUrl = 'https://www.easy-mock.com/mock/5a0bf56bdbfe9e4cbd641706/unmanned'
-let authUrl = 'https://www.easy-mock.com/mock/59f02babb120c445fab92be2/account'
+// api相关
+let baseUrl = 'http://apitest.example.com' // api接口
 
 if (env === 'production') { // 生产环境
   htmlTitle = 'Vue Admin'
-  baseUrl = '//api.example.com'
-  authUrl = '//auth.example.com'
+  baseUrl = 'http://api.example.com'
+  authUrl = 'http://auth.example.com'
 } else if (env === 'testing') { // 测试环境
   htmlTitle = 'Vue Admin - test'
-  baseUrl = '//apitest.example.com'
-  authUrl = '//authtest.example.com'
+  authUrl = 'http://116.62.148.115:8082'
+  baseUrl = 'http://apitest.example.com'
 }
 
 module.exports = {
   gitDir,
+  htmlTitle,
   clientId,
   redirectUri,
   authCodes,
-  htmlTitle,
-  baseUrl,
   authUrl,
+  baseUrl,
 }
