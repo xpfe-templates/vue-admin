25dfed83fe98450090a831c1df929150fb6546b0
xiaoping <edwardhjp@gmail.com>
2018-01-30 11:39:25 +0800

    fix 修复分页

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -5,7 +5,7 @@
 >
   <div class="el-slot" style="display: inline-block;">
     <span class="el-pagination__jump">
-      前往<input class="el-pagination__editor" type="text" :min="1" v-model="jumpPage">页
+      前往<input class="el-pagination__editor" type="text" :min="1" v-model="jumpPage" @keyup.enter.stop="onJump">页
     </span>
     <el-button class="el-pagination__btn" type="primary" @click="onJump">Go</el-button>
   </div>
@@ -25,16 +25,72 @@ export default {
 
   methods: {
     onJump () {
-      let jumpPage = Number(this.jumpPage)
-      if (!Number.isInteger(jumpPage)) {
-        this.jumpPage = jumpPage = 1
+      this.jumpPage = +this.jumpPage
+      let page = this.jumpPage
+      if (!Number.isInteger(page)) {
+        this.jumpPage = page = 1
       }
-      this.$emit('jump-change', jumpPage)
+      this.onPageChange(page)
     },
     onPageChange (page) {
+      if (this.currentPage === page) return // 解决多次触发
       this.$emit('update:currentPage', page)
       this.$listeners['current-change'](page)
     },
   },
 }
 </script>
+
+<style>
+.pagination {
+  margin-top: 10px;
+  text-align: right;
+}
+.el-pager li {
+  padding: 0 4px;
+  background: #fff;
+  vertical-align: top;
+  display: inline-block;
+  font-size: 13px;
+  min-width: 36px;
+  height: 28px;
+  line-height: 28px;
+  cursor: pointer;
+  box-sizing: border-box;
+  text-align: center;
+  margin: 0;
+}
+.el-pager li:hover {
+  color: #1CCADA;
+}
+.el-pager li.active {
+  color: #1CCADA;
+}
+.el-pagination {
+  padding: 12px 0;
+  text-align: right;
+}
+.el-pagination__btn {
+  margin-left: 12px;
+  background-color: #1CCADA;
+  color: #fff;
+}
+.el-pagination__btn:hover {
+  background: #1FE1F2 !important;
+  color: #fff !important;
+}
+.el-pagination.is-active, .el-pagination:active {
+  color: #19B1BF;
+}
+.el-pagination__editor {
+  border: 1px solid #d1dbe5;
+  border-radius: 2px;
+  line-height: 18px;
+  padding: 4px 2px;
+  width: 36px;
+  text-align: center;
+  margin: 0 6px;
+  box-sizing: border-box;
+  transition: border .3s;
+}
+</style>
